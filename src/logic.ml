6ec4504e9ee4dc02e632ec53a802e29bc224ba00
Zach Cheslock <zcc9@cornell.edu>
2023-04-23 12:40:46 -0400

    Update Logic to uncomment out code and use new function names from Command

--- src/logic.ml
+++ src/logic.ml
@@ -1,9 +1,8 @@
-(**
-    (** [pow x y] returns [x^y] *)
+(** [pow x y] returns [x^y] *)
 let rec two_pow x = if x = 0 then 1 else 2 * two_pow (x - 1)
 
 (** Get a list of the names of all Propositions in an expression *)
-let rec get_props (e : Command.expression) : char list =
+let rec get_props (e : Command.logic_expression) : char list =
   match e with
   | Prop c -> [ c ]
   | Dis (e1, e2) -> get_props e1 @ get_props e2
@@ -15,8 +14,8 @@ let rec get_props (e : Command.expression) : char list =
 (** Determines if an expression is true or false, given the truth values of all
     it's propositions. Requires: [prop_vals] contains a [prop, value] pair for
     every prop in [get_props e]*)
-let rec eval_prop (prop_vals : (char * bool) list) (e : Command.expression) :
-    bool =
+let rec eval_prop (prop_vals : (char * bool) list)
+    (e : Command.logic_expression) : bool =
   match e with
   | Prop c -> List.assoc c prop_vals
   | Dis (e1, e2) -> eval_prop prop_vals e1 || eval_prop prop_vals e2
@@ -39,7 +38,7 @@ let rec prop_vals_from_config (props : char list) (config : int) :
   | [] -> []
 
 (** Compares two propositions for the case specified by config *)
-let compare_case (e1 : Command.expression) (e2 : Command.expression)
+let compare_case (e1 : Command.logic_expression) (e2 : Command.logic_expression)
     (props : char list) (config : int) : bool =
   let prop_vals = prop_vals_from_config props config in
   eval_prop prop_vals e1 = eval_prop prop_vals e2
@@ -47,13 +46,12 @@ let compare_case (e1 : Command.expression) (e2 : Command.expression)
 (** Returns: true if and only if e1 and e2 have the same truth value for the
     case specified by config and for all cases specified by a lower-number
     config*)
-let rec compare_rec (e1 : Command.expression) (e2 : Command.expression)
-    (props : char list) (config : int) : bool =
+let rec compare_rec (e1 : Command.logic_expression)
+    (e2 : Command.logic_expression) (props : char list) (config : int) : bool =
   if not (compare_case e1 e2 props config) then false
   else if config > 0 then compare_rec e1 e2 props (config - 1)
   else true
 
-let compare (e1 : Command.expression) (e2 : Command.expression) =
+let compare (e1 : Command.logic_expression) (e2 : Command.logic_expression) =
   let props = get_props e1 @ get_props e2 |> List.sort_uniq Stdlib.compare in
   compare_rec e1 e2 props (two_pow (List.length props) - 1)
-  *)
--- src/logic.mli
+++ src/logic.mli
@@ -1,6 +1,6 @@
-(** (** Determining logical equality of expressions *)
+(** Determining logical equality of expressions *)
 
-    val compare : Command.expression -> Command.expression -> bool (** Determine
-    if two expressions are equal. Two expressions are equal if they have the
-    same truth values for any possible configurations of the truth values of
-    their propositions. *) *)
+val compare : Command.logic_expression -> Command.logic_expression -> bool
+(** Determine if two expressions are equal. Two expressions are equal if they
+    have the same truth values for any possible configurations of the truth
+    values of their propositions. *)
