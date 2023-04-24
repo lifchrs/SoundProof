4dd315725efa2c406610d5ae4c95665e37446053
Zach Cheslock <zcc9@cornell.edu>
2023-04-24 11:03:43 -0400

    Add functionality for validation of subset steps
    - Implement function compare_sets and helper functions
    - Update logic of compare_logic to use implication rather than equality
    - Restructure helper methods for compare_logic to enable code reuse

--- src/logic.mli
+++ src/logic.mli
@@ -1,6 +1,9 @@
 (** Determining logical equality of expressions *)
 
-val compare : Command.logic_expression -> Command.logic_expression -> bool
+val compare_logic : Command.logic_expression -> Command.logic_expression -> bool
 (** Determine if two expressions are equal. Two expressions are equal if they
     have the same truth values for any possible configurations of the truth
     values of their propositions. *)
+
+val compare_sets : Command.set_expression -> Command.set_expression -> bool
+(** [compare_sets A B] is true if and only if [A] is a subset of [B]*)
