901ee13b5b24a32917aaf8108a781c3803f4ab63
Andre Oganesian <ao367@cornell.edu>
2023-04-24 11:18:42 -0400

    Minor refactoring

--- bin/main.ml
+++ bin/main.ml
@@ -1,59 +1,132 @@
-(** open Verifier open Command
+open Verifier
+open Command
 
-    type command = | Assume of expression | Show of expression | Verify of
-    expression
+type command =
+  | Assume of logic_expression
+  | Show of logic_expression
+  | Verify of logic_expression
 
-    let help_message = "Thank you for using Proof Verifier supreme. \n" ^ "To
-    input your proof, you will enter the statement you want to show, your \
-    given assumptions, and the steps between. \n" ^ "The supported syntax is as
-    follows: \n" ^ " -Show E: Goal is to prove E true \n" ^ " -Assume P: Set P
-    as true \n" ^ " -Verify R: Use R as next step of proof \n" ^ "Any of E, P,
-    R, must only contain conjugation, disjunction, implication, \
-    bi-implication, or negation.\n" ^ "These are denoted by \"^\", \"v\",
-    \"=>\", \"<=>\", \"!\" respectively\n" ^ "Finally, use proper parentheses to
-    certify order of operations. For \ example, \n" ^ "\"(P=>Q)^!R\" and
-    \"P=>(Q^!R)\" are both proper syntax with different \ meanings. \n" ^ "Good
-    luck and happy proving!"
+let help_message =
+  "Thank you for using Proof Verifier supreme. \n"
+  ^ "To input your proof, you will enter the statement you want to show, your \
+     given assumptions, and the steps between. \n"
+  ^ "The supported syntax is as follows: \n"
+  ^ " -Show E: Goal is to prove E true \n" ^ " -Assume P: Set P as true \n"
+  ^ " -Verify R: Use R as next step of proof \n"
+  ^ "Any of E, P, R, must only contain conjugation, disjunction, implication, \
+     bi-implication, or negation.\n"
+  ^ "These are denoted by \"^\", \"v\", \"=>\", \"<=>\", \"!\" respectively\n"
+  ^ "Finally, use proper parentheses to certify order of operations. For  \
+     example, \n"
+  ^ "\"(P=>Q)^!R\" and \"P=>(Q^!R)\" are both proper syntax with different  \
+     meanings. \n" ^ "Good luck and happy proving!"
 
-    (** Causes string to appear like it is printing by letter instead of
-    appearing *) let type_out_slowly str = let len = String.length str in let
-    rec loop i = if i < len then ( print_char str.[i]; flush stdout; (*Adjust
-    value of Unix.select to change typing speed*) ignore (Unix.select [] [] []
-    0.01); loop (i + 1)) in loop 0; print_newline ()
+(** Causes string to appear like it is printing by letter instead of appearing *)
+let type_out_slowly str =
+  let len = String.length str in
+  let rec loop i =
+    if i < len then (
+      print_char str.[i];
+      flush stdout;
+      (*Adjust value of Unix.select to change typing speed*)
+      ignore (Unix.select [] [] [] 0.01);
+      loop (i + 1))
+  in
+  loop 0;
+  print_newline ()
 
-    (** Checks if input is valid, and returns proper command if it is.
-    Otherwise, asks the user to re-input a command *) let rec get_command _ =
-    let str = try read_line () with End_of_file -> print_endline "Exiting now.";
-    exit 0 in if String.lowercase_ascii str = "quit" then ( print_endline
-    "Exiting now."; exit 0) else if String.lowercase_ascii str = "help" then (
-    type_out_slowly help_message; get_command ()) else let lst_with_empty =
-    String.split_on_char ' ' str in let lst = List.fold_right (fun x acc -> if x
-    <> "" then x :: acc else acc) lst_with_empty [] in try match lst with | []
-    \-> raise Empty | h :: t -> if h = "Assume" then Assume (parse t) else if h
-    = "Show" then Show (parse t) else if h = "Verify" then Verify (parse t) else
-    raise Malformed with | Malformed -> type_out_slowly "Error, Malformed string
-    detected. Please make sure you enter a \ keyword followed by an expression.
-    Type \"help\" for help or \ \"quit\" to quit"; get_command () | Empty ->
-    type_out_slowly "Error, Empty string detected. Please make sure you enter a
-    keyword \ followed by an expression. Type \"help\" for help or \"quit\" to \
-    quit"; get_command ()
+(** Checks if input is valid, and returns proper command if it is. Otherwise,
+    asks the user to re-input a command *)
+let rec get_command _ =
+  let str =
+    try read_line ()
+    with End_of_file ->
+      print_endline "Exiting now.";
+      exit 0
+  in
+  if String.lowercase_ascii str = "quit" then (
+    print_endline "Exiting now.";
+    exit 0)
+  else if String.lowercase_ascii str = "help" then (
+    type_out_slowly help_message;
+    get_command ())
+  else
+    let lst_with_empty = String.split_on_char ' ' str in
+    let lst =
+      List.fold_right
+        (fun x acc -> if x <> "" then x :: acc else acc)
+        lst_with_empty []
+    in
+    try
+      match lst with
+      | [] -> raise Empty
+      | h :: t ->
+          if h = "Assume" then Assume (parse_logic t)
+          else if h = "Show" then Show (parse_logic t)
+          else if h = "Verify" then Verify (parse_logic t)
+          else raise Malformed
+    with
+    | Malformed ->
+        type_out_slowly
+          "Error, Malformed string\n\
+          \    detected. Please make sure you enter a  keyword followed by an \
+           expression.\n\
+          \    Type \"help\" for help or  \"quit\" to quit";
+        get_command ()
+    | Empty ->
+        type_out_slowly
+          "Error, Empty string detected. Please make sure you enter a\n\
+          \    keyword  followed by an expression. Type \"help\" for help or \
+           \"quit\" to quit";
+        get_command ()
 
-    (** Repeatedly asks the user for an input and checks if it is equivalent to
-    the previous input. Currently only terminates upon entering "quit" or an
-    invalid step*) let rec proof_loop _ = match get_command () with | Assume e
-    \-> Proof.add_to_history e true; proof_loop () | Show e ->
-    Proof.set_current_goal (Some e); proof_loop () | Verify e ->
-    Proof.add_to_history e false; proof_loop ()
+(** Repeatedly asks the user for an input and checks if it is equivalent to the
+    previous input. Currently only terminates upon entering "quit" or an invalid
+    step*)
+let rec proof_loop _ =
+  match get_command () with
+  | Assume e ->
+      Proof.add_to_history e true;
+      proof_loop ()
+  | Show e ->
+      Proof.set_current_goal (Some e);
+      proof_loop ()
+  | Verify e ->
+      Proof.add_to_history e false;
+      proof_loop ()
 
-    (** [main ()] prompts for the game to play, then starts it. *) let rec main
-    () = match String.lowercase_ascii (try read_line () with End_of_file ->
-    print_endline "Exiting now."; exit 0) with | "help" -> type_out_slowly
-    help_message; main () | "start" -> type_out_slowly "Beginning proof process.
-    Remember, if at any point you need help, \ just type the command \"help\"";
-    let _ = proof_loop () in () | "quit" -> print_endline "Exiting now."; exit 0
-    | _ -> type_out_slowly "Unknown command. If you want to start your proof,
-    type \"start\", or \ \"help\" for more information"; main ()
+(** [main ()] prompts for the game to play, then starts it. *)
+let rec main () =
+  match
+    String.lowercase_ascii
+      (try read_line ()
+       with End_of_file ->
+         print_endline "Exiting now.";
+         exit 0)
+  with
+  | "help" ->
+      type_out_slowly help_message;
+      main ()
+  | "start" ->
+      type_out_slowly
+        "Beginning proof process.\n\
+        \    Remember, if at any point you need help,  just type the command \
+         \"help\"";
+      let _ = proof_loop () in
+      ()
+  | "quit" ->
+      print_endline "Exiting now.";
+      exit 0
+  | _ ->
+      type_out_slowly
+        "Unknown command. If you want to start your proof,\n\
+        \    type \"start\", or  \"help\" for more information";
+      main ()
 
-    (* Execute the game engine. *) let () = print_endline ""; type_out_slowly
+(* Execute the game engine. *)
+let () =
+  print_endline "";
+  type_out_slowly
     "Please use the command \"start\" to begin your proof, or \"help\" for \
-    more information"; main () *)
+     more information";
+  main ()
--- src/proof.ml
+++ src/proof.ml
@@ -1,25 +1,70 @@
-(* let current_goal : Command.logic_expression option ref = ref None let history
-   : Command.logic_expression list ref = ref []
+let current_goal : Command.logic_expression option ref = ref None
+let history : Command.logic_expression list ref = ref []
+let compare_all e = List.map (fun x -> Logic.compare e x) !history
 
-   let compare_all e = List.map (fun x -> if Logic.compare e x then true else (
-   print_endline ("Error! Entered expression is not logically equivalent to:" ^
-   Command.string_of_logic_expr x); false)) !history
+(** [pp_string s] pretty-prints string [s]. *)
+let pp_string s = "\"" ^ s ^ "\""
 
-   let set_current_goal e = match e with | None -> current_goal := None | Some
-   e1 -> if (* Checks if the string of any expression is identical to the one we
-   are trying to show*) List.exists (fun str -> String.equal str
-   (Command.string_of_logic_expr e1)) (List.map (fun e2 ->
-   Command.string_of_logic_expr e2) !history) then ( print_endline "Goal already
-   shown! Please enter a new goal"; current_goal := None) else current_goal := e
+(** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt] to
+    pretty-print each element of [lst]. *)
+let pp_list pp_elt lst =
+  let pp_elts lst =
+    let rec loop n acc = function
+      | [] -> acc
+      | [ h ] -> acc ^ pp_elt h
+      | h1 :: (_ :: _ as t') ->
+          if n = 100 then acc ^ "..." (* stop printing long list *)
+          else loop (n + 1) (acc ^ pp_elt h1 ^ "; ") t'
+    in
+    loop 0 "" lst
+  in
+  "[" ^ pp_elts lst ^ "]"
 
-   let compare_with_goal e = match !current_goal with | None -> () | Some exp ->
-   if String.equal (Command.string_of_logic_expr e)
-   (Command.string_of_logic_expr exp) then ( print_endline "Goal shown! Please
-   enter a new goal"; current_goal := None)
+let set_current_goal e =
+  match e with
+  | None -> current_goal := None
+  | Some e1 ->
+      if
+        (* Checks if the string of any expression is identical to the one we are
+           trying to show*)
+        List.exists
+          (fun str -> String.equal str (Command.string_of_logic_expr e1))
+          (List.map (fun e2 -> Command.string_of_logic_expr e2) !history)
+      then (
+        print_endline "Goal already shown! Please enter a new goal";
+        current_goal := None)
+      else current_goal := e
 
-   let verify_new_step e = if List.for_all (fun x -> x) (compare_all e) then (
-   history := !history @ [ e ]; compare_with_goal e) else print_endline "Entered
-   step not logically equivalent to previous steps. Please enter a \ new step"
+let compare_with_goal e =
+  match !current_goal with
+  | None -> ()
+  | Some exp ->
+      if
+        String.equal
+          (Command.string_of_logic_expr e)
+          (Command.string_of_logic_expr exp)
+      then (
+        print_endline "Goal shown! Please enter a new goal";
+        current_goal := None)
 
-   let add_to_history e force = if force then ( compare_with_goal e; history :=
-   !history @ [ e ]) else verify_new_step e *)
+let verify_new_step e =
+  let temp = compare_all e in
+  if List.exists (fun x -> x) temp then (
+    history := !history @ [ e ];
+    compare_with_goal e)
+  else
+    print_endline "Entered step not logically equivalent to any previous step";
+  print_endline
+    ("Previous steps: "
+    ^ pp_list pp_string
+        (List.map (fun x -> Command.string_of_logic_expr x) !history));
+  print_endline
+    ("Equivalence: "
+    ^ pp_list pp_string (List.map (fun x -> if x then "True" else "False") temp)
+    )
+
+let add_to_history e force =
+  if force then (
+    compare_with_goal e;
+    history := !history @ [ e ])
+  else verify_new_step e
