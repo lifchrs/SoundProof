22e342fb323ecea963e460e9016d8dcaeadfc484
Adhitya Polavaram <ap977@cornell.edu>
2023-04-30 03:16:55 -0400

    Add set parsing test cases

--- test/main.ml
+++ test/main.ml
@@ -69,9 +69,9 @@ let parse_logic_tests =
     parse_logic_test "chained implication" "((A)=>((B)=>(C)))" "A => (B => C)";
     parse_logic_test "biimplication with and" "((A)<=>((B)^(C)))"
       "A <=> (B ^ C)";
-    parse_logic_test "commutativity of or" "(((P)^(B))=>((B)^(P)))"
+    parse_logic_test "commutativity of and" "(((P)^(B))=>((B)^(P)))"
       "(P ^ B) => (B ^P)";
-    parse_logic_test "Biimpl with or" "(((U)^(P))=>((U)<=>(P)))"
+    parse_logic_test "Biimpl with and" "(((U)^(P))=>((U)<=>(P)))"
       "(U^P) => (U <=> P)";
     parse_logic_test "implication with not" "((B)=>(!(P)))" "B => !P";
     parse_logic_test "bimplication implies implication"
@@ -94,7 +94,7 @@ let parse_logic_tests =
       "!(A <=> B)";
     parse_logic_test "implication implies implication"
       "(((A)=>(B))=>((C)=>(D)))" "(A=>B) => (C => D)";
-    parse_logic_test "testing different prop naems with ands of ors"
+    parse_logic_test "testing different prop names with ands of ors"
       "(((A)v(B))^(((C)v(D))^((E)v(D))))" "(AvB)^((CvD)^(EvD ))";
     parse_logic_test "bimplication with implication" "((A)<=>((B)=>(C)))"
       "A <=> (B => C)";
@@ -103,7 +103,7 @@ let parse_logic_tests =
     parse_logic_test "ors of negations" "(!((C)v(!((D)v(!(A))))))"
       "!(C) v !(D) v !A";
     parse_logic_test "neg of and" "(!((A)^(B)))" "!(A^B)";
-    parse_logic_test "and of neg" "((!(A))v(B))" "((!A) v (B))";
+    parse_logic_test "or of neg" "((!(A))v(B))" "((!A) v (B))";
     parse_logic_test "peels parentheses correctly" "(A)" "((((((((A))))))))";
     parse_logic_test "ors with precedence" "(((A)v(B))v(C))" "(((A) v B) v C)";
     parse_logic_test "A or A" "((A)v(A))" "A  v  A";
@@ -112,7 +112,7 @@ let parse_logic_tests =
     parse_logic_test "multiple negations" "(!(!(!(!(!(!(A)))))))"
       "!(!(!(!(!(!(A))))))";
     parse_logic_test "multiple (fewer) negations" "(!(!(!(A))))" "!(!(!A))";
-    parse_logic_test "nested netations " "(!((B)^(!((B)^(!(B))))))"
+    parse_logic_test "nested negations " "(!((B)^(!((B)^(!(B))))))"
       "!(B^!(B^!(B)))";
     parse_logic_test "nested implications" "((A)=>((B)=>((C)=>((D)=>(E)))))"
       "A => (B => (C => (D => E)))";
@@ -125,7 +125,7 @@ let parse_logic_tests =
       "(!C) => (!D)";
     parse_logic_test "negation of an implication using A" "(!((A)=>(A)))"
       "!(A => A)";
-    parse_logic_error_test "unbalanced parentehses" Malformed [ "()))" ];
+    parse_logic_error_test "unbalanced parentheses" Malformed [ "()))" ];
     parse_logic_error_test "double A (AA)" Malformed [ "AA" ];
     parse_logic_error_test "unbalanced parentheses with A" Malformed [ "((A)" ];
     parse_logic_error_test "singularly empty parens" Malformed [ "()" ];
@@ -220,6 +220,108 @@ let parse_sets_tests =
       [ "(()" ];
     parse_set_error_test "raises malformed with incorrect expression AB"
       Malformed [ "AB" ];
+    parse_set_error_test "unbalanced parentheses" Malformed [ "()))" ];
+    parse_set_error_test "double A (AA)" Malformed [ "AA" ];
+    parse_set_error_test "unbalanced parentheses with A" Malformed [ "((A)" ];
+    parse_set_error_test "singularly empty parens" Malformed [ "()" ];
+    parse_set_error_test "nested empty parens" Malformed [ "(())" ];
+    parse_set_error_test "parens around lowercase" Malformed [ "(a)" ];
+    parse_set_error_test "AB" Malformed [ "AB" ];
+    parse_set_error_test "double union (vv) " Malformed [ "AvvB" ];
+    parse_set_error_test "double inters (^^)" Malformed [ "A ^^ C" ];
+    parse_set_error_test "BB in a larger expression" Malformed
+      [ "A v (BB ^ D)" ];
+    parse_set_error_test "singular union" Malformed [ "v" ];
+    parse_set_error_test "singular inters" Malformed [ "^" ];
+    parse_set_error_test "singular diff" Malformed [ "\\" ];
+    parse_set_error_test "singular Comp" Malformed [ "Comp" ];
+    parse_set_error_test "inters then diff" Malformed [ "A ^ \\ B" ];
+    parse_set_error_test "union then diff" Malformed [ "A v \\ E" ];
+    parse_set_error_test "triple inters" Malformed [ "A ^^^ B" ];
+    parse_set_error_test "AB in chain of inters" Malformed [ "A ^ AB ^ D" ];
+    parse_set_error_test "diff at end" Malformed [ "B\\" ];
+    parse_set_error_test "trailing inion" Malformed [ "Av" ];
+    parse_set_error_test "incomplete inside parens" Malformed [ "(Bv)A^A" ];
+    parse_set_error_test "beginning with union" Malformed [ "vA^V" ];
+    parse_set_error_test "incomplete outside parens" Malformed [ "C(D^(AvA))" ];
+    parse_set_error_test "Set Comp Set" Malformed [ "B Comp D" ];
+    parse_set_error_test "inters then diff" Malformed [ "A => (D^\\D)" ];
+    parse_set_error_test "empty parens next to eachother" Malformed
+      [ "()()(A)" ];
+    parse_set_error_test "empty parens with lower case" Malformed [ "()()(a)" ];
+    parse_set_error_test "just empty parens" Malformed [ "()()()" ];
+    parse_set_error_test "empty parens before correct expr" Malformed
+      [ "(()) A v B" ];
+    parse_set_test "Comp of chained inters" "(Comp((A)^((B)^((C)^(B)))))"
+      "Comp(A^B^C^B)";
+    parse_set_test "multiple union" "((A)v((B)v((C)v(D))))" "A v B v C v D";
+    parse_set_test "Diff, inters, union" "((A)\\((V)^((C)v(D))))" "A \\ V^(CvD)";
+    parse_set_test "Comp of union with inters" "(Comp((A)^((B)v(D))))"
+      "Comp(A ^ BvD)";
+    parse_set_test "Comp of union with inters" "(Comp((A)v(((B)^(C))^(D))))"
+      "Comp(A v ((B ^ C)^D))";
+    parse_set_test "5 chained unions" "((A)v((B)v((C)v((D)v(E)))))"
+      "A v B v C v D v E";
+    parse_set_test "Complement of intersection using A" "(Comp((A)^(A)))"
+      "Comp(A ^ A)";
+    parse_set_test "Diff with A and A" "((A)\\(A))" "A \\ A";
+    parse_set_test "comp of unions" "(Comp((A)v(Comp(B))))" "Comp(A v CompB)";
+    parse_set_test "Diff with inters" "((A)\\((B)^(C)))" "A \\ (B ^ C)";
+    parse_set_test "commutativity of inters" "(((P)^(B))^((B)^(P)))"
+      "(P ^ B) ^ (B ^P)";
+    parse_set_test "Diff with unions" "(((U)^(P))^((U)\\(P)))"
+      "(U^P) ^ (U \\ P)";
+    parse_set_test "Complement of an intersection" "((B)^(Comp(P)))"
+      "B ^ Comp P";
+    parse_set_test
+      "intersection intersected with union (different set letters too)"
+      "(((A)^(D))^((B)v(D)))" "(A ^ D) ^ (B v D)";
+    parse_set_test "comp inters and unions with precedence"
+      "(Comp((A)^((A)v((D)^(A)))))" "Comp(A ^ (A v D ^ A))";
+    parse_set_test "(C) different set names" "(C)" "C";
+    parse_set_test "(D) different set names" "(D)" "D";
+    parse_set_test "complement of the chain of unions"
+      "(Comp((A)v((A)v((A)v(A)))))" "Comp(A v A v A v A)";
+    parse_set_test "chain of inters" "((A)^((A)^((A)^((A)^((A)^(A))))))"
+      "A ^ A ^A^ A ^ A^A";
+    parse_set_test "chain of inters with parentheses precedence"
+      "(((A)^(A))^(A))" "(A^A) ^ A";
+    parse_set_test "unions of V" "((V)v((V)v(V)))" "V v V v V";
+    parse_set_test "unions and intersections of V" "((V)v((V)^(V)))"
+      "(V) v V ^ V";
+    parse_set_test "inters with different precedence" "(((A)^(B))^((C)^(D)))"
+      "(A^B) ^ (C ^ D)";
+    parse_set_test "testing different set names with inters of unions"
+      "(((A)v(B))^(((C)v(D))^((E)v(D))))" "(AvB)^((CvD)^(EvD ))";
+    parse_set_test "set difference with intesection" "((A)\\((B)^(C)))"
+      "A \\ (B ^ C)";
+    parse_set_test "Comp of union and with inters" "(Comp(((A)v(D))^((C)^(D))))"
+      "Comp(AvD) ^ C^D";
+    parse_set_test "unions of Comp" "(Comp((C)v(Comp((D)v(Comp(A))))))"
+      "Comp(C) v Comp(D) v CompA";
+    parse_set_test "Comp of inters" "(Comp((A)^(B)))" "Comp(A^B)";
+    parse_set_test "union of comp" "((Comp(A))v(B))" "((Comp A) v (B))";
+    parse_set_test "peels parentheses correctly (A with multiple parens" "(A)"
+      "((((((((A))))))))";
+    parse_set_test "unions with precedence using parens" "(((A)v(B))v(C))"
+      "(((A) v B) v C)";
+    parse_set_test "A union A" "((A)v(A))" "A  v  A";
+    parse_set_test "spacing doesn't matter" "((B)^((A)v(D)))" "B ^   (A v    D)";
+    parse_set_test "multiple complements"
+      "(Comp(Comp(Comp(Comp(Comp(Comp(A)))))))"
+      "Comp(Comp(Comp(Comp(Comp(Comp(A))))))";
+    parse_set_test "multiple (fewer) comps" "(Comp(Comp(Comp(A))))"
+      "Comp(Comp(CompA))";
+    parse_set_test "nested comps" "(Comp((B)^(Comp((B)^(Comp(B))))))"
+      "Comp(B^Comp(B^Comp(B)))";
+    parse_set_test "intersection of Comp of intersection"
+      "((A)^(Comp((B)^(A))))" "A ^ Comp(B^A)";
+    parse_set_test "Diff of union + inters" "((A)\\((B)^((C)v(A))))"
+      "A \\ B ^ (C v A) ";
+    parse_set_test "Inters of Diff" "((A)^((B)\\(C)))" "A ^ (B \\ C)";
+    parse_set_test "intersection of Comp" "((Comp(C))^(Comp(D)))"
+      "(CompC) ^ (CompD)";
+    parse_set_test "Comp of inters using A" "(Comp((A)^(A)))" "Comp(A ^ A)";
   ]
 
 (* Testing for Logic compilation unit *)
